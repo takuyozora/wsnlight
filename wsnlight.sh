1cc028199ad8584727aefe097439f3be7fcb68dd
Olivier Radisson <olivier.radisson@insa-lyon.fr>
2016-01-20 21:04:08 +0000

    add lock in starting wsnlight procedure

--- wsnlight.sh
+++ wsnlight.sh
@@ -1,12 +1,18 @@
 #!/bin/bash
 
+while [ -e /tmp/wsnlight.lock ]; do
+    sleep 1     # unable to run vicking when lock (for test)
+done
+
 while [ ! "$(ps -ejH | grep olad)" ]; do
     sleep 1
 done
 sleep 3     # ensure olad is really started
 
 echo "Starting WSN light..."
+touch /tmp/wsnlight.lock
 ./wsnlight.py
+rm /tmp/wsnlight.lock
 
 exit $?
 
